1dc41228e95ac2f6faf5f4ae0fdb7be297d3497a
vito.palmisano <vito.palmisano.e@wolterskluwer.com>
2023-04-17 15:10:08 +0200

    first commit

---
+++ Commands/MethodToPhraseCommand.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+
+namespace PhraseToMethod
+{
+	//[Command(PackageIds.MethodToPhraseCommand)]
+	//internal sealed class MethodToPhraseCommand : BaseCommand<MethodToPhraseCommand>
+	//{
+	//	protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+	//	{
+	//		var docView = await VS.Documents.GetActiveDocumentViewAsync();
+	//		var selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();
+
+	//		if (selection.HasValue)
+	//		{
+	//			docView.TextBuffer.Replace(selection.Value, "da metodo a frase");
+	//		}
+	//	}
+	//}
+
+	internal sealed class Command1
+	{
+		public const int CommandId = 0x0101;
+
+		public static readonly Guid CommandSet = new Guid("bea4976d-811a-4dd0-9745-0bed7a658b5d");
+
+		private readonly Package package;
+
+		private Command1(Package package)
+		{
+			if (package == null)
+			{
+				throw new ArgumentNullException("package");
+			}
+
+			this.package = package;
+
+			OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+			if (commandService != null)
+			{
+				var menuCommandID = new CommandID(CommandSet, CommandId);
+				var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+				commandService.AddCommand(menuItem);
+			}
+		}
+
+		public static Command1 Instance
+		{
+			get;
+			private set;
+		}
+
+		private IServiceProvider ServiceProvider
+		{
+			get
+			{
+				return this.package;
+			}
+		}
+
+		public static void Initialize(Package package)
+		{
+			Instance = new Command1(package);
+		}
+
+		private void MenuItemCallback(object sender, EventArgs e)
+		{
+			string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+			string title = "Command1";
+
+			// Show a message box to prove we were here
+			VsShellUtilities.ShowMessageBox(
+				 this.ServiceProvider,
+				 message,
+				 title,
+				 OLEMSGICON.OLEMSGICON_INFO,
+				 OLEMSGBUTTON.OLEMSGBUTTON_OK,
+				 OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+		}
+	}
+}
---
+++ PhraseToMethodPackage.cs
@@ -0,0 +1,27 @@
+﻿global using Community.VisualStudio.Toolkit;
+
+global using Microsoft.VisualStudio.Shell;
+
+global using System;
+
+global using Task = System.Threading.Tasks.Task;
+
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace PhraseToMethod
+{
+	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+	[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+	[ProvideMenuResource("Menus.ctmenu", 1)]
+	[Guid(PackageGuids.PhraseToMethodString)]
+	public sealed class PhraseToMethodPackage : ToolkitPackage
+	{
+		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+		{
+			await this.RegisterCommandsAsync();
+			Command1.Initialize(this);
+			Command2.Initialize(this);
+		}
+	}
+}
